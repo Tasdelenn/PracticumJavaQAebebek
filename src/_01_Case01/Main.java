1018ec4a9bec236ca1b8e5c5d61ce6556606e017
HaQaN <hakann.tasdelenn@gmail.com>
2022-10-18 19:37:14 +0300

    Bazı Düzeltmeler Yapıldı: Güncellenen task'a göre, bazı metodlarda ve çıktı görünümünde değişiklikler yapıldı.

--- src/_01_Case01/Main.java
+++ src/_01_Case01/Main.java
@@ -4,16 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Hakan Tasdelen",100000,40,2021);
-        employees[1] = new Employee("Ali Yeşil", 60000,50,2018);
-        employees[2] = new Employee("Ayşe Sarı", 80000,36,2020);
-        employees[3] = new Employee("Mehmet Turuncu", 90000,48,2011);
-        employees[4] = new Employee("Şükrü Kırmızı", 120000,48,2005);
+        employees[0] = new Employee("Hakan Taşdelen", 100000, 40, 2001);
+        employees[1] = new Employee("Ali Yeşil", 60000, 50, 2018);
+        employees[2] = new Employee("Ayşe Sarı", 80000, 36, 2020);
+        employees[3] = new Employee("Mehmet Turuncu", 90000, 48, 2011);
+        employees[4] = new Employee("Şükrü Kırmızı", 2000, 45, 1985);
 
         for (Employee emp : employees) {
             System.out.println(emp.toString());
         }
-
 
     }
 }
